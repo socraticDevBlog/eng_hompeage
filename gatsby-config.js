e09d5137de76551a5501c518ce0614e6661f77c9
socraticDevBlog <maximebonin@outlook.com>
2019-11-16 13:44:55 -0500

    changed config to my name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Maxime Bonin | Homepage",
+    author: "Maxime Bonin",
+    description: "my english homepage"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
