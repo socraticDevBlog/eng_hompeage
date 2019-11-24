6e5af9492f53249f93c67bb30f520e45feb91709
socraticDevBlog <maximebonin@outlook.com>
2019-11-24 15:43:06 -0500

    added blog section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,57 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
-            <span className="icon fas fa-terminal"></span>
-        </div>
-        <div className="content">
-            <div className="inner">
-                <h1></h1>
-                <p>Homepage to this Maxime Bonin who is a software and web developer</p>
-            </div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Philosophy</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Career</a></li>
-            </ul>
-        </nav>
-    </header>
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fas fa-terminal"></span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1></h1>
+        <p>Homepage to this Maxime Bonin who is a software and web developer</p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Philosophy
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Career
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('blog')
+            }}
+          >
+            Blog
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
-    onOpenArticle: PropTypes.func,
-    timeout: PropTypes.bool
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
 }
 
 export default Header
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import philosophie from '../images/philosophy.jpg'
 import scrum from '../images/scrum.gif'
+import blog from '../images/blog.jpg'
 
 class Main extends React.Component {
   render() {
@@ -36,21 +37,31 @@ class Main extends React.Component {
             target="_blank"
           >
             <span className="image main">
-              <img src={philosophie} alt="screenshot of my master's thesis in french" />
+              <img
+                src={philosophie}
+                alt="screenshot of my master's thesis in french"
+              />
             </span>
           </a>
           <p>
-            Loving thick and complex books from an early age, I had discovered the joys of abstract thinking.
-            I've developped a taste to formal logic, philosophy of science and analytical philosophy being an undergrad student at Laval University.
-            Coming from Arts and Humanities background, I've dedicated more efforts into logic and science to polish the rational side of my mind.
+            Loving thick and complex books from an early age, I had discovered
+            the joys of abstract thinking. I've developped a taste to formal
+            logic, philosophy of science and analytical philosophy being an
+            undergrad student at Laval University. Coming from Arts and
+            Humanities background, I've dedicated more efforts into logic and
+            science to polish the rational side of my mind.
           </p>
           <p>
-            Aiming at becoming a Philosophy teacher, I went into grad school in Montreal. I've completed a thesis about the concept of sophism thru the lense dialectics, linguistics and pragmatics.
-            I have been a teacher for a short while and chose to pivot into technology.
+            Aiming at becoming a Philosophy teacher, I went into grad school in
+            Montreal. I've completed a thesis about the concept of sophism thru
+            the lense dialectics, linguistics and pragmatics. I have been a
+            teacher for a short while and chose to pivot into technology.
           </p>
           <p>
-            Since then philosophy had become a hobby to me. Between being an exercise of abstract thinking and an art of living.
-            I've rediscovered the fundamentals : Plato and the philosophers of the Antiquity.
+            Since then philosophy had become a hobby to me. Between being an
+            exercise of abstract thinking and an art of living. I've
+            rediscovered the fundamentals : Plato and the philosophers of the
+            Antiquity.
           </p>
           {close}
         </article>
@@ -75,20 +86,67 @@ class Main extends React.Component {
             </span>
           </a>
           <p>
-           After a short stint as a teacher, I've taken the challenge of a professional retraining.
-           I wanted to be part of this technological revolution of the 21st century. Putting my critical thinking skills to work and developping concrete projects.
-           Becoming a software developer !
+            After a short stint as a teacher, I've taken the challenge of a
+            professional retraining. I wanted to be part of this technological
+            revolution of the 21st century. Putting my critical thinking skills
+            to work and developping concrete projects. Becoming a software
+            developer !
           </p>
           <p>
-            Out of college, I've worked with the R & D department of a small software company.
-            First maintaining their medical office solution. Then dedicating all my time developping a mobile app and
-            a restful web service.
+            Out of college, I've worked with the R & D department of a small
+            software company. First maintaining their medical office solution.
+            Then dedicating all my time developping a mobile app and a restful
+            web service.
           </p>
           <p>
-            I'm currently working as a consultant for a large governmental agency. 
-            I'm part of a team that takes care of several systems used throughout the agency for business purposes.
-            I like to think this role enables me to shine a light onto my leadership qualities as well as my technical skills. 
-            Solving problems and developing new features using an array of technologies : web apps, web services, sql scripting, batch processes, etc.
+            I'm currently working as a consultant for a large governmental
+            agency. I'm part of a team that takes care of several systems used
+            throughout the agency for business purposes. I like to think this
+            role enables me to shine a light onto my leadership qualities as
+            well as my technical skills. Solving problems and developing new
+            features using an array of technologies : web apps, web services,
+            sql scripting, batch processes, etc.
+          </p>
+          {close}
+        </article>
+        <article
+          id="blog"
+          className={`${this.props.article === 'blog' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Blog</h2>
+          <a
+            href="https://en.socratic.dev"
+            alt="url pointing to the english version of my blog"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <span className="image main">
+              <img src={blog} alt="screenshot of a blog post blurb" />
+            </span>
+          </a>
+          <p>
+            Writing is still the best way to share ideas. I'm sharing subject
+            matters I'm interested in thru short blog posts. Most of the time,
+            those posts are linked to software and web developement. However,
+            philosophy and humanism drive a good part of my digital
+            interventions.
+          </p>
+          <p>
+            The best way to truly master a subject matter, one has to explain it
+            ; teach it. I get a lot of value back from maintaining this blog :)
+          </p>
+          <p>
+            <a
+              href="https://en.socratic.dev"
+              alt="url to english version of my blog"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              https://en.socratic.dev
+            </a>
           </p>
           {close}
         </article>
